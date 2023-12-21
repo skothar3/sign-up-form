b238b05ea7a3d97727bff36684a75c3a77613797
Sid Kothari <sid.kothari7@gmail.com>
2023-12-21 08:35:59 -0500

    Add skeleton of for loop to add event listeners to form inputs

--- validation.js
+++ validation.js
@@ -1,3 +1,16 @@
 const form = document.querySelector("form");
+const inputs = form.elements;
+const errors = document.querySelectorAll("span.error");
+// console.log(inputs);
+// console.log(errors);
 
-console.log(form.elements);
+for (let element of inputs) {
+  if (element.name.match(/^phone|submit$/)) {
+    console.log(element);
+    element.addEventListener("input",(event) => {
+      if (element.validity.valid) {
+
+      }
+    } )
+  }
+}
